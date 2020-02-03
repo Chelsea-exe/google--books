1def1f98de2a84240c4db87c8320862327570b31
Chelsea-exe <cjackson145@student.gsu.edu>
2020-02-03 13:49:09 -0500

    added axios npm and function for google API search, add, and delete books

---
+++ src/APIs/googleAPI.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+export default {
+    searchBooks: function(query) {
+      return axios.get("https://www.googleapis.com/books/v1/volumes?q="+ query);
+    },
+    saveBook: function (bookData) {
+      return axios.post("/api/books" , bookData);  
+    },
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id)
+    }
+};
